602f49270d65c3de0f185a031d12922723cfe4b3
CrouchingJahi <crouching.jahi@gmail.com>
2025-10-29 11:26:58 -0400

    Fixed some bugs in TransactionMatcher

--- src/components/transactionCategories/TransactionMatcher.jsx
+++ src/components/transactionCategories/TransactionMatcher.jsx
@@ -1,17 +1,20 @@
 
 export default function TransactionMatcher({stringMatchers, categories, transactionName}) {
-  const matches = stringMatchers.filter(regex => transactionName.matchAll(regex.pattern)).map(matcher => {
-    return {
-      ...matcher,
-      category: categories.find(cat => cat.id == matcher.categoryId),
-    }
-  })
+  const matches = transactionName.length == 0 ? [] :
+    stringMatchers
+      .filter(regex => transactionName.match(regex.pattern))
+      .map(matcher => {
+        return {
+          ...matcher,
+          category: categories.find(cat => cat.id == matcher.categoryId),
+        }
+      })
   return <div>
     { matches.length > 0 ?
       <div>
         { matches.length } { matches.length == 1 ? 'match' : 'matches' } found:
         <ul>
-          { matches.map(matcher => <li>{ matcher.pattern } &rarr; { matcher.category.catName }</li>) }
+          { matches.map(matcher => <li key={matcher.id}>{ matcher.pattern } &rarr; { matcher.category.catName }</li>) }
         </ul>
       </div> :
       <div>No matches found.</div>
